a9016efad4377a7bfb92619456c369a7372f9e8e
Dean <dean.sha@gmail.com>
2019-03-26 18:51:40 +0200

    eclipse built-in git test

--- DragonTale/src/main/Main.java
+++ DragonTale/src/main/Main.java
@@ -4,30 +4,30 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 import javax.swing.JFrame;
+
 public class Main {
 
 	public static void main(String[] args) {
-			//System.setProperty("sun.java2d.opengl", "true");
-			JFrame obj = new JFrame();
-			Gameplay gameplay = new Gameplay();
-			obj.setBounds(10, 10, GameConstants.WIDTH, GameConstants.HEIGHT);
-			obj.add(gameplay);
-			//obj.pack();
-			obj.setTitle("Dragon Tale");
-			obj.setResizable(false);
-			obj.setVisible(true);
-			//obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			obj.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-			obj.addWindowListener(new WindowAdapter() {
-			        @Override
-			        public void windowClosing(WindowEvent event) {
-			        	gameplay.stop();
-			        	System.exit(0);
-			        }
-			    });
-			gameplay.start();
-			
-	}
-	
-}
+		// System.setProperty("sun.java2d.opengl", "true");
+		JFrame obj = new JFrame();
+		Gameplay gameplay = new Gameplay();
+		obj.setBounds(10, 10, GameConstants.WIDTH, GameConstants.HEIGHT);
+		obj.add(gameplay);
+		// obj.pack();
+		obj.setTitle("Dragon Tale");
+		obj.setResizable(false);
+		obj.setVisible(true);
+		// obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		obj.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		obj.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent event) {
+				gameplay.stop();
+				System.exit(0);
+			}
+		});
+		gameplay.start();
 
+	}
+
+}
